48549fc28d90945f036f4b32bf9f8c9d8335bd0a
Asher Lien <asher.lien@riversoft.com.tw>
2024-06-14 09:48:52 +0800

    更新

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,18 @@
+<script setup lang="ts">
+const state = ref({
+  form: {
+    grade: null,
+    school: '',
+    height: null,
+    weigth: null,
+    figure: null,
+  },
+})
+</script>
+
 <template>
   <div>
-    <p b b-3 text-50px>
+    <p b b-3 text-20px>
       test
     </p>
   </div>
